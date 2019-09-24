17bf47f801ed12e475a69f84262acb5ab8581c3f
I326319 <ranganath.sn@gmail.com>
2019-09-24 18:38:43 +0530

    Building the component

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent{
+    // add property for products list
+}
